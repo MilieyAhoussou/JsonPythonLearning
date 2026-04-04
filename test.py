e41df2e8bfedb1d54d42bfd227027ef272d33e11
Ahoussou Miliey <ahoussoumiliey@gmail.com>
2026-04-04 22:58:26 +0000

    Enhance JSON handling in test.py: added functionality to convert dictionaries to JSON and back, including file operations and type checks.

--- test.py
+++ test.py
@@ -38,5 +38,31 @@ dog_data = {
   ],
 }
 
+#the following line open/create a file called data.json and convert the dictionary into a json array
 with open("data.json", mode="w", encoding="utf_8") as write_file:
     json.dump(dog_data, write_file)
+
+dog_registry = {1:{"name": "Frieda"}}
+#we turn the dictionary into a json object
+dog_json = json.dumps(dog_registry)
+print(dog_json)
+#we turn the json object into a python object
+new_dog_registry = json.loads(dog_json)
+#this line return false, it looks like we lost the primary format of our dictionary
+print(dog_registry == new_dog_registry)
+#in json, the keys of an object are str but in python they canbe whatever.
+#so we see that we we convert the element into a json object the key that was a int became a str
+print(new_dog_registry)
+
+dog_data_json = json.dumps(dog_data)
+print(dog_data_json)
+new_dog_data = json.loads(dog_data_json)
+print(new_dog_data)
+
+#convert a json file into a python type object
+with open("data.json", mode="r", encoding="utf_8") as read_file:
+    frie_data = json.load(read_file)
+
+print(frie_data)
+print(type(frie_data))
+print(frie_data["name"])
